2b582e5ff97fa0a422213095e4a4aebd2267630b
unknown <alina.leshchenko@infis.cz>
2025-03-19 13:01:51 +0100

    adsadas

--- ukol1ukol2.py
+++ ukol1ukol2.py
@@ -1,4 +1,22 @@
-rok = 2025
-text = "Vesele Velikonoce"
+jmeno = input("Zadejte sve jmeno: ")
+vek = input("Zadejte svuj vek: ")
+print(f"Ahoj, jmenuji se {jmeno} a je mi {vek} let.")
 
-print(f"Ahoj, {text} {rok}")
+
+cislo1 = float(input("Zadejte prvni desetinne cislo: "))
+cislo2 = float(input("Zadejte druhe desetinne cislo: "))
+soucet = cislo1+cislo2
+rozdil = cislo1-cislo2
+soucin = cislo1*cislo2
+if cislo2 !=0:
+    podil = cislo1/cislo2
+else:
+    podil = None
+
+print(f"Soucet: {soucet: .2f}")
+print(f":Rozdil: {rozdil: .2f}")
+print(f":Soucin: {soucin: .2f}")
+if podil is not None:
+    print(f"Podil: {podil: .2f}")
+else:
+    print("Podil nelze vypocitat (deleni nulou).")
